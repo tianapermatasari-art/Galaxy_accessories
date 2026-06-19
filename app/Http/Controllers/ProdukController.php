940588ccba36a68a38c26be4a7306d488e02d1ce
tianapermatasari-art <tianapermatasari@saintek.unipdu.ac.id>
2026-06-19 18:34:53 +0700

    percobaan lagi

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -4,35 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class ProdukController extends Controller
 {
     public function index()
     {
         return 'Laravel berhasil berjalan';
-    }
-
-    public function create()
-    {
-        return 'Halaman Create';
-    }
-
-    public function store(Request $request)
-    {
-        return 'Store berhasil';
-    }
-
-    public function edit($id)
-    {
-        return 'Edit produk ID: ' . $id;
-    }
-
-    public function update(Request $request, $id)
-    {
-        return 'Update produk ID: ' . $id;
-    }
-
-    public function destroy($id)
-    {
-        return 'Hapus produk ID: ' . $id;
     }
 }
