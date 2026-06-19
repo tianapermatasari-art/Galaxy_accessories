3b58d846a77c70deb70140886d277a7b8a7231ef
tianapermatasari-art <tianapermatasari@saintek.unipdu.ac.id>
2026-06-19 16:29:40 +0700

    fix vercel php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,15 @@
 <?php
-// Mencegah error terkait path
-chdir(__DIR__ . '/..');
 
-// Jalankan Laravel
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
